1a7bd14c710c318515f24e35706f2dacb59195a5
Jakub Zawadzki <jakubzawadzkii@interia.pl>
2025-02-27 20:41:40 +0100

    Layout changes and redirections

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,27 @@
+using GymTrack.Areas.Identity.Data;
 using GymTrack.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
 using System.Diagnostics;
 
 namespace GymTrack.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<GymUser> _userManager;
+        public HomeController(ILogger<HomeController> logger, UserManager<GymUser> userManager)
         {
+            _userManager = userManager;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
             return View();
         }
 
