6aee36fc7bc84735df0198dcc76f1e0e03a4c867
Jakub Zawadzki <axcvbf617@gmail.com>
2025-07-06 21:01:35 +0200

    Excercise Form Added

--- Controllers/TrainingController.cs
+++ Controllers/TrainingController.cs
@@ -1,12 +1,22 @@
-﻿using GymTrack.Models;
+﻿using GymTrack.Areas.Identity.Data;
+using GymTrack.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymTrack.Controllers
 {
     public class TrainingController : Controller
     {
+        private readonly UserManager<GymUser> UserManager;
+
+        public TrainingController(UserManager<GymUser> userManager)
+        {
+            UserManager = userManager;
+        }
         public IActionResult Create(DateTime date)
         {
+            ViewData["UserID"] = UserManager.GetUserId(this.User);
+
             var model = new TrainingViewModel
             {
                 Date = date,
