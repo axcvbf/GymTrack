8b858cd53361630a294f77405cf6d08d54c0c7b0
Jakub Zawadzki <jakubzawadzkii@interia.pl>
2025-08-03 12:04:36 +0200

    Days with workout saved are now marked with green glow

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 using GymTrack.Areas.Identity.Data;
+using GymTrack.Data;
+using GymTrack.Models;
 using GymTrack.ViewModels;
+using Humanizer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
 using System.Diagnostics;
 
@@ -11,25 +15,39 @@ namespace GymTrack.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly UserManager<GymUser> _userManager;
-        public HomeController(ILogger<HomeController> logger, UserManager<GymUser> userManager)
+        private readonly ILogger<HomeController> Logger;
+        private readonly UserManager<GymUser> UserManager;
+        private readonly GymDbContext Context;
+        public HomeController(ILogger<HomeController> logger, UserManager<GymUser> userManager, GymDbContext context)
         {
-            _userManager = userManager;
-            _logger = logger;
+            Context = context;
+            UserManager = userManager;
+            Logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewData["UserID"] = _userManager.GetUserId(this.User);
+            ViewData["UserID"] = UserManager.GetUserId(this.User);
 
             int month = Request.Query["month"].Count > 0 ? int.Parse(Request.Query["month"]) : DateTime.Now.Month;
             int year = Request.Query["year"].Count > 0 ? int.Parse(Request.Query["year"]) : DateTime.Now.Year;
 
-            DateTime currentMonth = new DateTime(year, month, 1);
+            DateTime currentDate = new DateTime(year, month, 1);
 
-            ViewBag.Month = currentMonth;
-            return View();
+            var user = await UserManager.GetUserAsync(User);
+            var trainings = await Context.Trainings
+                .Where(t => t.GymUserId == user.Id &&
+                            t.Date.Month == currentDate.Month &&
+                            t.Date.Year == currentDate.Year)
+                .ToListAsync();
+
+            var model = new HomeViewModel
+            {
+                currentDate = currentDate,
+                trainingDays = trainings.Select(t => t.Date.Date).ToList()
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GymTrack.ViewModels
+{
+    public class HomeViewModel
+    {
+        public DateTime currentDate { get; set; }
+        public List<DateTime> trainingDays { get; set; }
+    }
+}
